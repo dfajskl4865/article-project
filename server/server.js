f6fde1ee9d8cb7546d1281e1954b866859fdd6c8
김근태 <dfajskl4865@gmail.com>
2022-11-14 19:44:16 +0900

    article-project

---
+++ server/server.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+
+// ========== DB연결 수행 전 라이브러리 호출 ===============
+const mysql = require("mysql2");
+const { Form } = require("react-router-dom");
+const db = mysql.createPoolCluster();
+// ========== DB연결 수행 전 라이브러리 호출 ===============
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true, // 도에인이 달라도 쿠키 공유 받아주는 것
+  })
+);
+
+db.add("article_project", {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "article_project",
+  port: 3306,
+});
+
+/**
+ *  비동기를 동기로 바꾸려면
+ *  Promise 객체로 묶어줘야함
+ */
+
+function 디비실행(query) {
+  return new Promise(function (resolve, reject) {
+    db.getConnection("article_project", function (error, connection) {
+      if (error) {
+        console.log("디비 연결 오류", error);
+        reject(true);
+      }
+
+      connection.query(query, function (error, data) {
+        if (error) {
+          console.log("쿼리오류", error);
+          reject(true);
+        }
+
+        resolve(data);
+      });
+      connection.release();
+    });
+  });
+}
+
+app.get("/", async (req, res) => {
+  const 데이터 = await 디비실행("SELECT * FROM user");
+
+  console.log(데이터);
+
+  res.send("여기로 옵니다!!");
+});
+
+app.post("/join", async (req, res) => {
+  const { id, pw } = req.body;
+
+  const result = {
+    code: "success",
+    message: "회원가입되었습니다",
+  };
+
+  // Mysql user 테이블에 INSERT 해줘야함
+
+  const 회원 = await 디비실행(`SELECT * FROM user WHERE id = '${id}'`);
+
+  if (회원.length > 0) {
+    result.code = "error";
+    result.message = "이미 같은 아이디로 가입되었습니다";
+    res.send(result);
+    return;
+  }
+
+  const query = `INSERT INTO user(id,password,nickname) VALUES('${id}','${pw}','지나가던나그네')`;
+  await 디비실행(query);
+
+  res.send(result);
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.session.loginUser);
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.session);
+
+  res.send("//");
+});
+
+app.post("/login", async (req, res) => {
+  const { id, pw } = req.body;
+  /**
+   *  1. 아이디랑 비밀번호가 같은 데이터가 있는지 확인 (Mysql DB)
+   *   - 같은 데이터가 존재하면 session 저장
+   *   - 없을 경우 [회원 정보가 존재하지 않습니다] 메시지 보내주기
+   */
+
+  const result = {
+    code: "success",
+    message: "로그인 되었습니다",
+  };
+
+  const 회원 = await 디비실행(
+    `SELECT * FROM user WHERE id = '${id}' AND password = '${pw}'`
+  );
+
+  if (회원.length === 0) {
+    result.code = "error";
+    result.message = "회원 정보가 존재하지 않습니다.";
+    res.send(result);
+    return;
+  }
+
+  req.session.loginUser = 회원[0];
+  req.session.save();
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log("서버가 시작되었습니다");
+});
---
+++ src/App.js
@@ -0,0 +1,145 @@
+import axios from "axios";
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+
+// 도에인이 달라도 쿠키 공유해주는 것
+axios.defaults.withCredentials = true;
+
+function Join() {
+  const navigation = useNavigate();
+
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 회원가입 = async () => {
+    /**
+     * GET : 서버에서 가져올 때 사용
+     * POST : 어떤것을 만들 때 사용
+     */
+    await axios({
+      url: "http://localhost:4000/join",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        const { code, message } = res.data;
+        if (code === "success") {
+          alert(message);
+          navigation("/login");
+        }
+      })
+      .catch((e) => {
+        console.log("join Error", e);
+      });
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <input type="text" name="id" onChange={데이터변경} />
+      <input type="password" name="pw" onChange={데이터변경} />
+      <button type="button" onClick={회원가입}>
+        회원가입
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  const navigation = useNavigate();
+
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 로그인 = async () => {
+    await axios({
+      url: "http://localhost:4000/login",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        alert(res.data.message);
+
+        if (res.data.code === "success") {
+          window.location.href = "/";
+        }
+      })
+      .catch((e) => {
+        console.log("로그인에러", e);
+      });
+  };
+
+  return (
+    <div>
+      <input name="id" onChange={데이터변경} />
+      <input name="pw" onChange={데이터변경} />
+      <button type="button" onClick={로그인}>
+        로그인
+      </button>
+    </div>
+  );
+}
+
+function Main() {
+  return <div>Main</div>;
+}
+
+/**
+ *
+ * 1. React Router DOM
+ * 2. Context provider
+ */
+
+const StoreContext = React.createContext({});
+
+function App() {
+  const [loginUser, setLoginUser] = React.useState({});
+
+  const 세션정보가져오기 = async () => {
+    await axios({
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      setLoginUser(res.data);
+    });
+  };
+
+  React.useEffect(() => {
+    세션정보가져오기();
+  }, []);
+
+  return (
+    <StoreContext.Provider
+      value={{
+        loginUser,
+      }}
+    >
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/join" element={<Join />} />
+      </Routes>
+    </StoreContext.Provider>
+  );
+}
+
+export default App;
